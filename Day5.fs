6230ecff4db289636b2ab2301f63d61a4d7b8ed8
Craig Wilson <craig@wilsoncg.net>
2022-12-16 01:14:16 +0000

    day5 - test passes

--- Day5.fs
+++ Day5.fs
@@ -6,24 +6,35 @@ open System.Text
 
 type MoveInstruction =
   // move (number of crates) from (stack i) to (stack j)
-  | Item of int * int * int
+  | Instruction of int * int * int
 
 let private MoveStacks moves (stacks: list<Stack<string>>) = 
-  let regexp = @"(\d+)"
+  let regexp = "move (\d+) from (\d+) to (\d+)"
   let toInt s = Int32.Parse s
-  let toMoveInstruction (g:RegularExpressions.GroupCollection) =
-    let n = g[0].Value |> toInt
-    let i = g[1].Value |> toInt
-    let j = g[2].Value |> toInt
-    Item(n, i, j)
-  let regexMatch s =
-    let m = RegularExpressions.Regex.Match(regexp, s) 
+  let toInstruction (g:RegularExpressions.GroupCollection) =
+    let n = g[1].Value |> toInt
+    let i = g[2].Value |> toInt
+    let j = g[3].Value |> toInt
+    Instruction(n, i, j)
+  let toMoveInstruction s =
+    let m = RegularExpressions.Regex.Match(s, regexp, RegularExpressions.RegexOptions.Compiled) 
     match m with
-    | m when m.Success -> Some(toMoveInstruction m.Groups)
+    | m when m.Success -> Some(toInstruction m.Groups)
     | _ -> None
+  let popThenPush n i j =
+    let popped = [
+      for a in 1..n do
+       yield stacks[i-1].Pop()
+    ]
+    Seq.iter (fun f -> stacks[j-1].Push(f)) popped
+    stacks
   moves
-  |> Seq.map (fun f -> regexMatch f)
-  stacks
+  |> Seq.map toMoveInstruction
+  |> Seq.choose id
+  |> Seq.map (fun (mi:MoveInstruction) ->
+    match mi with
+    | Instruction (n,i,j) -> popThenPush n i j
+  )
 
 let CrateStacking (i:string) (seperator:string) skipN stackSetup =
     let moves = 
@@ -32,5 +43,6 @@ let CrateStacking (i:string) (seperator:string) skipN stackSetup =
       |> Array.where (fun f -> not(String.IsNullOrEmpty f))
     let moved =
       MoveStacks moves stackSetup
-      |> Seq.map (fun f -> f.Pop())
-    String.Join ("", moved)
+      |> Seq.last
+    let result = List.map (fun (f:Stack<string>) -> f.Pop()) moved
+    String.Join ("", result)
